a7a765a2546653ce1f27ec4dbac08c741aa56618
piyush singh <123636271+itspiyush78@users.noreply.github.com>
2024-03-11 23:42:24 +0530

    all setup done

---
+++ Drawing a Shape/Drawing a Shape/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drawing_a_Shape
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("   |---|");
+            Console.WriteLine("   |   |");
+            Console.WriteLine("   |___|");
+            Console.WriteLine("   |");
+            Console.WriteLine("   |");
+            Console.WriteLine("   |");
+            Console.ReadLine();
+        }
+    }
+}
