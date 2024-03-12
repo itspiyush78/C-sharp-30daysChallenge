b1631ca5f71e281ee9aea81f4913069f794fb995
piyush singh <123636271+itspiyush78@users.noreply.github.com>
2024-03-12 23:41:22 +0530

    variables done

---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string characterName = "Gaurav";
+            int characterAge;
+            characterAge = 20;
+            Console.WriteLine(characterName + " is a good coder.");
+            Console.WriteLine("He is " + characterAge + " years old.");
+            Console.ReadLine();
+        }
+    }
+}
