45a74dc3c0fe5c239e907d868b24bdc9a473bcf1
piyush singh <123636271+itspiyush78@users.noreply.github.com>
2024-03-22 11:17:00 +0530

    string done

---
+++ String/String/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Dhruv\nAcademy");
+            string phrase = "Dhruv Academy";
+            Console.WriteLine(phrase);
+            Console.ReadLine();
+        }
+    }
+}
