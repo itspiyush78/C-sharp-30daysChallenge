68bf29bf9ddf614024605340b2513203de51372b
piyush singh <123636271+itspiyush78@users.noreply.github.com>
2024-03-26 22:29:08 +0530

    numbers done

---
+++ Numbers/Numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(2 + 2);
+            Console.WriteLine(4 + 2 * 3);
+            Console.WriteLine((4 + 2) * 3);
+            Console.WriteLine(Math.Sqrt(81));
+            Console.WriteLine(Math.Max(8, 87));
+            Console.WriteLine(Math.Round(5.2));
+            Console.ReadLine();
+        }
+    }
+}
