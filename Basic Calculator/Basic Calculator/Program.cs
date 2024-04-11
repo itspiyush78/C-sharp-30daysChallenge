eee7fa1fd587eba52546992390daebaad0623606
piyush singh <123636271+itspiyush78@users.noreply.github.com>
2024-04-11 10:38:32 +0530

    Basic calculator done

---
+++ Basic Calculator/Basic Calculator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter another number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(num1 + num2);
+            Console.ReadLine();
+        }
+    }
+}
+
