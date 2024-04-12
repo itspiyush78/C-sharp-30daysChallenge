644f0db1adc7e9b447a2547f41a91f399d351b63
piyush singh <123636271+itspiyush78@users.noreply.github.com>
2024-04-12 14:34:12 +0530

    mad lib done

---
+++ Building a mad lib/Building a mad lib/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Building_a_mad_lib
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string color, pluralNoun, celebrity;
+
+            Console.WriteLine("Enter a color: ");
+            color = Console.ReadLine();
+
+
+            Console.WriteLine("Enter a plural noun: ");
+            pluralNoun = Console.ReadLine();
+
+
+            Console.WriteLine("Enter a celebrity: ");
+            celebrity = Console.ReadLine();
+
+            Console.WriteLine("Roses are " + color);
+            Console.WriteLine(pluralNoun + " are blue");
+            Console.WriteLine("I Love " + celebrity);
+
+            Console.ReadLine();
+        }
+    }
+}
