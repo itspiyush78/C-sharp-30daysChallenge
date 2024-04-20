5512962a10bcb52d8dbc0eb10ec46817287b6bc0
piyush singh <123636271+itspiyush78@users.noreply.github.com>
2024-04-20 12:37:15 +0530

    methods done

---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SayHi("Akash", 23);
+            SayHi("Arun", 22);
+            SayHi("Rahul", 20);
+
+            Console.ReadLine();
+        }
+
+        static void SayHi(string name, int age)
+        {
+            Console.WriteLine("Hello " + name + " you are " + age);
+        }
+    }
+}
