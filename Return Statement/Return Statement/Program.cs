c4a987c975294dbf2235de8861d38ce2dfb79c50
piyush singh <123636271+itspiyush78@users.noreply.github.com>
2024-04-22 11:15:07 +0530

    return statement

---
+++ Return Statement/Return Statement/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Return_Statement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cubeNumber = cube(6);
+            Console.WriteLine(cubeNumber);
+            Console.ReadLine();
+        }
+        static int cube(int num) 
+        {
+            int result = num * num * num;
+            return result;
+        }
+    }
+}
