9ca6e793de1d1c367f92b209ed560dd1ba4c53fe
piyush singh <123636271+itspiyush78@users.noreply.github.com>
2024-04-23 19:45:32 +0530

    if statement done

---
+++ If Statements/If Statements/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace If_Statements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isMale = false;
+            bool isTall = true;
+
+            if (isMale && isTall) 
+            {
+                Console.WriteLine("You are a tall male");
+            } else if (isMale && !isTall){
+                Console.WriteLine("You are  short male");
+            } else if (!isMale && isTall){
+                Console.WriteLine("You are  not a male but you are tall");
+            } else
+            {
+                Console.WriteLine("You are not male and not tall");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
