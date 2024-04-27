8dbfce6fc8a1337bd61cbc808158e3b8b0146519
piyush singh <123636271+itspiyush78@users.noreply.github.com>
2024-04-27 15:17:42 +0530

    while loop done

---
+++ While_loop/While_loop/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While_loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int index = 1;
+            while (index <= 8)
+            {
+                Console.WriteLine(index);
+                index++;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
