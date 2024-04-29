1b683cc60bbb20cf5d04e63e51f4d5c250425de9
piyush singh <123636271+itspiyush78@users.noreply.github.com>
2024-04-29 13:13:45 +0530

    word guessing game

---
+++ Word  Guessing Game/Word  Guessing Game/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Word__Guessing_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string secretWord = "technology";
+            string guess = "";
+            int guessCount = 0;
+            int guessLimit = 4;
+            bool outofGuesses = false;
+
+            while (guess != secretWord && !outofGuesses)
+            {
+                if (guessCount < guessLimit)
+                {
+                    Console.Write("Enter guess: ");
+                    guess = Console.ReadLine();
+                    guessCount++;
+                }
+                else
+                {
+                    outofGuesses = true;
+                }
+            }
+            if (outofGuesses)
+            {
+                Console.Write("You Lose!😑");
+            }
+            else
+            {
+                Console.Write("You Win!☺");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
