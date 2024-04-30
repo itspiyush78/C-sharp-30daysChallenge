98f76d54cab22277e3d9b8c8fac76bf61eb5fb69
piyush singh <123636271+itspiyush78@users.noreply.github.com>
2024-04-30 14:24:51 +0530

    for loop done

---
+++ For Loop/For Loop/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For_Loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] luckyumbers = { 2, 8, 9, 10, 11, 12, 13, 14, };
+            for (int i = 0; i < luckyumbers.Length; i++)
+            {
+                Console.WriteLine(luckyumbers[i]);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
