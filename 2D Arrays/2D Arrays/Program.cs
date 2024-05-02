6dae56ce6388ef6d62891669450bc3d7f2a10c0b
piyush singh <123636271+itspiyush78@users.noreply.github.com>
2024-05-02 08:58:02 +0530

    2D arrays done

---
+++ 2D Arrays/2D Arrays/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2D_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] numberGrid =
+            {
+                {4, 2 },
+                {3, 7 },
+                {6, 8 },
+            };
+
+            Console.WriteLine(numberGrid[0,1]);
+
+
+            Console.ReadLine();
+        }
+    }
+}
