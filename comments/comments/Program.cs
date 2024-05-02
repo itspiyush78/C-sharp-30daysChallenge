b3b8947e29674f302363ab468d111a41350be6bd
piyush singh <123636271+itspiyush78@users.noreply.github.com>
2024-05-02 16:27:18 +0530

    comments done

---
+++ comments/comments/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace comments
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // yjgyuetjh ytyty ukture5 ure6
+            /* iygfu
+             * jhguuif
+             * dut
+             * yui7y
+             */
+            Console.WriteLine("io");
+            Console.ReadLine();
+        }
+    }
+}
